53fe1b1b7a49c4104bc640166432ddf2100737b0
David Edmundson <kde@davidedmundson.co.uk>
2011-05-11 12:31:45 +0100

    Add missing files Add flat model proxy to show contacts.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,12 +10,16 @@
 
 
 #include "accounts-model.h"
+#include "flat-model-proxy.h"
+#include "account-filter-model.h"
 
 
 MainWindow::MainWindow(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::MainWindow)
 {
+    Tp::registerTypes();
+
     ui->setupUi(this);
 
     KFileItem file(KUrl("/home/david/a.png"), "image/png", KFileItem::Unknown);
@@ -32,21 +36,30 @@ MainWindow::MainWindow(QWidget *parent) :
 
 
     Tp::AccountFactoryPtr  accountFactory = Tp::AccountFactory::create(QDBusConnection::sessionBus(),
-                                                                       Tp::Features() << Tp::Account::FeatureCore);
+                                                                       Tp::Features() << Tp::Account::FeatureCore
+                                                                       << Tp::Account::FeatureAvatar
+                                                                       << Tp::Account::FeatureProtocolInfo
+                                                                       << Tp::Account::FeatureProfile);
 
-    Tp::ConnectionFactoryPtr connectionFactory = Tp::ConnectionFactory::create(QDBusConnection::sessionBus());
-    Tp::ChannelFactoryPtr channelFactory = Tp::ChannelFactory::create(QDBusConnection::sessionBus());
+    Tp::ConnectionFactoryPtr connectionFactory = Tp::ConnectionFactory::create(QDBusConnection::sessionBus(),
+                                                                               Tp::Features() << Tp::Connection::FeatureCore
+                                                                               << Tp::Connection::FeatureRosterGroups
+                                                                               << Tp::Connection::FeatureRoster
+                                                                               << Tp::Connection::FeatureSelfContact);
 
     Tp::ContactFactoryPtr contactFactory = Tp::ContactFactory::create(Tp::Features()  << Tp::Contact::FeatureAlias
                                                                       << Tp::Contact::FeatureAvatarData
                                                                       << Tp::Contact::FeatureSimplePresence
                                                                       << Tp::Contact::FeatureCapabilities);
 
+    Tp::ChannelFactoryPtr channelFactory = Tp::ChannelFactory::create(QDBusConnection::sessionBus());
+
     m_accountManager = Tp::AccountManager::create(QDBusConnection::sessionBus(),
-                                                                      accountFactory,
-                                                                      connectionFactory,
-                                                                      channelFactory,
-                                                                      contactFactory);
+                                                  accountFactory,
+                                                  connectionFactory,
+                                                  channelFactory,
+                                                  contactFactory);
+
 
     connect(m_accountManager->becomeReady(), SIGNAL(finished(Tp::PendingOperation*)), SLOT(onAccountManagerReady()));
 }
@@ -71,5 +84,9 @@ void MainWindow::showIcon(const KFileItem &file)
 void MainWindow::onAccountManagerReady()
 {
     AccountsModel *model = new AccountsModel(m_accountManager, this);
-    ui->listView->setModel(model);
+    FlatModelProxy *flatProxyModel = new FlatModelProxy(model);
+    AccountFilterModel *filterModel = new AccountFilterModel(this);
+    filterModel->setSourceModel(flatProxyModel);
+    filterModel->filterOfflineUsers(true);
+    ui->listView->setModel(filterModel);
 }
