36c939a28d05a9415330dbab7b87986b84382da0
Daniele E. Domenichelli <daniele.domenichelli@gmail.com>
2012-02-23 12:40:48 +0100

    Set version in using CMake instead of hardcoding in main.cpp
    Also bump version to 0.3.60

--- main.cpp
+++ main.cpp
@@ -27,14 +27,14 @@
 #include <KFileDialog>
 #include <KDebug>
 
-
 #include "mainwindow.h"
+#include "version.h"
 
 int main(int argc, char *argv[])
 {
     KAboutData aboutData("ktp-send-file", 0,
                          ki18n("Telepathy Send File"),
-                         "0.3");
+                         KTP_SEND_FILE_VERSION);
     aboutData.addAuthor(ki18n("David Edmundson"), ki18n("Author"), "kde@davidedmundson.co.uk");
 
     KCmdLineArgs::init(argc, argv, &aboutData);
